1c3a3ba01c6e72ca72983ae6db432bf6a8e4d298
Eugene Shevko <evgeniy.shevko@gearheart.io>
2022-09-23 09:24:41 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const animationRemote = bodymovin.loadAnimation({
+  container: document.getElementById("svg__00"),
+  path: "https://assets9.lottiefiles.com/packages/lf20_hKebN8.json",
+  autoplay: true,
+  renderer: "svg",
+  loop: true,
+})
